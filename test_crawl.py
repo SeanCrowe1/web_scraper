2d3cb385f74398f8f91fe8d033c6261af4f02c6d
SeanCrowe1 <sean.crowe2002@gmail.com>
2026-05-27 16:45:31 +0100

    initialized project

---
+++ test_crawl.py
@@ -0,0 +1,30 @@
+import unittest
+from crawl import normalize_url
+
+class TestCrawl(unittest.TestCase):
+    def test_normalize_url_https_one(self):
+        input_url = "https://www.boot.dev/blog/path"
+        actual = normalize_url(input_url)
+        expected = "www.boot.dev/blog/path"
+        self.assertEqual(actual, expected)
+
+    def test_normalize_url_https_two(self):
+        input_url = "https://www.boot.dev/blog/path/"
+        actual = normalize_url(input_url)
+        expected = "www.boot.dev/blog/path"
+        self.assertEqual(actual, expected)
+
+    def test_normalize_url_http_one(self):
+        input_url = "http://www.boot.dev/blog/path"
+        actual = normalize_url(input_url)
+        expected = "www.boot.dev/blog/path"
+        self.assertEqual(actual, expected)
+
+    def test_normalize_url_http_two(self):
+        input_url = "http://www.boot.dev/blog/path/"
+        actual = normalize_url(input_url)
+        expected = "www.boot.dev/blog/path"
+        self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
